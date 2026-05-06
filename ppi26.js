546ad2f5ce985d276643dcd18647b5767529a1a6
emillypereira-ship-it <galvao.emilly@escolar.ifrn.edu.br>
2026-05-06 19:19:21 +0000

    Aula 06/05 - introdução a javascrit

---
+++ ppi26.js
@@ -0,0 +1,37 @@
+console.log("bem vindos a ppi ");
+// introdução java script
+
+// variáveis
+// var name = 'joão'; ou mais usado
+let name = "joão";
+let age = 18;
+let isStudent = true;
+// isS var booleana
+const PI = 5.14;
+// CONST = NÃO PODE MUDAR DE VALOR
+// PI = 4; errado
+console.log(PI);
+
+//  Operadores Aritiméticos
+let x = 5 + 5; //soma
+let y = "5" + 5; //concatenação
+let z = "hello" + 5;
+
+console.log(x, y, z);
+console.log(x**2, y, z); 
+console.log(typeof x); // vai dizer tipo de variável
+// ativar o formatador prettier
+// ALT + SHIFT + F
+// Operadores relacionais
+console.log(x != 2); // '!' diferença
+console.log('5' == 5); // compara tipo OU valor
+console.log('5' === 5); //compara tipo E valor '===' igual
+
+//  operadores de incremento
+ //ou '--'
+console.log(x++);
+console.log(x);
+console.log(++x);
+
+
+
