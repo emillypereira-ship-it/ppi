c482e6ba8f906014def189ee59059a83b865fd5e
emillypereira-ship-it <galvao.emilly@escolar.ifrn.edu.br>
2026-07-15 13:39:08 +0000

    commit 15/07

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return(
+        <header>
+            <h1>Programação para internet</h1>
+            <h2> Curso Técnico Integrado em Informática</h2>
+        </header>
+    )
+}
+
+export default Header;
